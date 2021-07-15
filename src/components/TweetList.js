5a931146d64efcbea1fd65f0cc82a8943e88feb9
Erica Winne <erica.winne@powerhrg.com>
2021-07-15 11:09:45 -0400

    done

--- src/components/TweetList.js
+++ src/components/TweetList.js
@@ -1,16 +1,16 @@
 import React from "react";
 import TweetItem from "./TweetItem";
 
-function TweetList(props) {
+function TweetList({user}) {
   return (
     <div className="ui segment">
       <div className="ui feed">
-        {props.user.tweets.map((tweet) => (
+        {user.tweets.map((tweet) => (
           <TweetItem
             key={tweet.id}
-            handleTweetLike={props.handleTweetLike}
-            handle={props.user.handle}
-            photo={props.user.photo}
+        //  handleTweetLike={handleTweetLike}
+            handle={user.handle}
+            photo={user.photo}
             tweet={tweet}
           />
         ))}
--- src/components/TweetsContainer.js
+++ src/components/TweetsContainer.js
@@ -5,18 +5,31 @@ import { users as userData } from "../data/data";
 
 function TweetsContainer() {
   const [users, setUsers] = useState(userData);
+  const [userId, setUserId] = useState(users[0].id)
 
-  console.log("In TweetsContainer, state is", users);
+  const handleChangeUser = (user) => {
+    setUserId(user.id)
+  }
+  const updatedMoreUser = () => {
+  const updatedUsers = users.map((user) => { if (user.id !== userId) return user
+    return {...user}
+  })
+
+    setUsers(updatedUsers)
+}
+
+  const grabUserId = users.find((user) => user.id===userId)
+
   return (
     <div className="ui main container">
       <div className="ui grid">
         <div className="six wide column">
           <h2 className="ui header">Users</h2>
-          <UserList users={users} />
+          <UserList users={users} onChangeUser={handleChangeUser} />
         </div>
         <div className="ten wide column">
           <h2 className="ui header">Tweets</h2>
-          <TweetList user={users[0]} />
+          <TweetList user={grabUserId} />
         </div>
       </div>
     </div>
